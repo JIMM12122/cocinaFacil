64e65b076d1e9d8769ce377caefc040690b5d0b5
nvbale <nvbalek@gmail.com>
2023-11-21 12:41:07 -0600

    fix: add environmental variables

--- client/sanity.js
+++ client/sanity.js
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client';
 import imageBuilder from '@sanity/image-url';
 
 const client = sanityClient({
-    projectId: 'npcegap4',
+    projectId: 'tgz0bo9w',
     dataset: 'production',
     useCdn: true,
     apiVersion: '2021-10-21',
