a660ff213a2c15aca0d73e892a3d4a9080667995
JIMM12122 <88638504+JIMM12122@users.noreply.github.com>
2023-11-22 15:13:13 -0600

    se agragan las pantallas de lista de platillos

--- client/navigation.js
+++ client/navigation.js
@@ -6,7 +6,8 @@ import ResturantScreen from './screens/ResturantScreen'
 import CartScreen from './screens/CartScreen'
 import PreparingOrderScreen from './screens/PreparingOrderScreen'
 import DeliveryScreen from './screens/DeliveryScreen'
-import DishDetail from './components/DishDetail'
+import Categories from './components/categories'
+import DishListScreen from './screens/DishListScreen'
 const Stack = createNativeStackNavigator()
 
 export default function Navigation() {
@@ -15,21 +16,35 @@ export default function Navigation() {
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name='Home' component={HomeScreen} />
         <Stack.Screen name='Resturant' component={ResturantScreen} />
-        <Stack.Screen name="DishDetail" component={DishDetail} /> 
+
         <Stack.Screen
           name='Cart'
           options={{ presentation: 'modal', headerShown: false }}
           component={CartScreen}
         />
+
         <Stack.Screen
           name='PreparingOrder'
           options={{ presentation: 'fullScreenModal', headerShown: false }}
           component={PreparingOrderScreen}
         />
+
         <Stack.Screen
           name='Delivery'
           options={{ presentation: 'fullScreenModal', headerShown: false }}
           component={DeliveryScreen}
+        />
+
+        <Stack.Screen
+          name='Categories'
+          options={{ presentation: 'modal', headerShown: false }}
+          component={Categories}
+        />
+
+        <Stack.Screen
+          name='DishListScreen'
+          options={{ presentation: 'modal', headerShown: false }}
+          component={DishListScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
