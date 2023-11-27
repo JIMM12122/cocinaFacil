150091260a3facb1028040760ff366c57a89e62f
Wander1521 <evander.jimenez.jimenez@est.una.ac.cr>
2023-11-26 18:59:15 -0600

    Actualización

--- client/components/suggestedDishes.js
+++ client/components/suggestedDishes.js
@@ -4,9 +4,9 @@ export default function suggestedDishes() {
     return (
         <>
             <View className="bg-red-50 flex justify-center items-center flex-col">
-                <View>
-                    <Text className="flex flex-row justify-between" >Platillos sugeridos</Text>
-                    <Text>Platillos sugeridos</Text>
+                <View className="flex flex-row justify-between w-full">
+                    <Text className=" font-bold text-lg" >Platillos sugeridos</Text>
+                    <Text className="text-red-400 text-sm">Ver todo {'>'}</Text>
                 </View>
                 <View>
 
